006fddd79790c284df97d11d5afbc1316e9585fa
gapgit <dlarkqrl4966@gmail.com>
2019-09-20 23:47:08 +0900

    Login decorator imple, Quiz model, QuizView edit
    로그인 데코레이터 구현 및 퀴즈 모델 수정
    - choices, answer class 생성
    
    퀴즈 모델 수정으로 인한 QuizView 수정
    
    하나의 퀴즈를 생성할 시
    -> 퀴즈 생성
    -> 선택지 생성
    -> 퀴즈와 답안 선택지 연결 후 row 생성
    
    Review 반영

--- quiz/views.py
+++ quiz/views.py
@@ -1,26 +1,48 @@
 import json
 
-from .models        import Question
+from .models        import Question, Choices, Answer
+from user.utils     import user_login_required, advertiser_login_required
 
 from django.views   import View
 from django.http    import HttpResponse, JsonResponse
 
-class QuizView(View):
-    
+class QuizView(View): 
+    @advertiser_login_required
     def post(self, request):
         try:
             data = json.loads(request.body)
-    
-            Question.objects.create(
-                    advertisement_id=data['ad_id'],
-                    question_type_id=2,
-                    answer_type_id=2,
-                    answer=data['answer'],
-                    choices=data['choices'],
-                    title=data['title'],
-                    content=data['content'],
-                    )
+            
+            for quiz in data['quizzes']:
+                created_quiz = Question.objects.create(
+                    advertisement_id  = data['ad_id'],
+                    question_type_id  = 2,
+                    answer_type_id    = 2,
+                    title             = quiz['title'],
+                    content           = quiz['content'],
+                )
 
+                choices = [
+                    Choices(
+                        question = created_quiz, 
+                        content  = content
+                    ) 
+                    for content in quiz['choices']
+                ]
+                Choices.objects.bulk_create(choices)
+
+                if len(quiz['answer']) > 1:
+                    answers = [
+                        Answer(
+                            question   = created_quiz, 
+                            choices_id = answer
+                        )
+                        for answer in quiz['answer']
+                    ]
+                    Answer.objects.bulk_create(answers)
+                else:
+                    answer_choice_id = Choices.objects.get(question = created_quiz, content = quiz['answer'][0])
+                    Answer.objects.create(question = created_quiz, choices = answer_choice_id)
+                    
             return HttpResponse(status=200)
         except KeyError:
-            return JsonResponse({"ERROR":"MISSING_DATA"}, status=400)            
+            return JsonResponse({"ERROR":"MISSING_DATA"}, status=400)
---
+++ user/utils.py
@@ -0,0 +1,53 @@
+import jwt
+
+from .models          import User, Advertiser
+from adwards.settings import SECRET_KEY
+
+from django.http      import JsonResponse
+
+def user_login_required(func):
+    def decorated_function(self, request, *args, **kwargs):
+        access_token = request.headers.get('Authorization', None)
+
+        if access_token:
+            try:
+                payload = jwt.decode(access_token, SECRET_KEY, 'HS256')
+                user_type = payload['user_type']
+
+                if user_type == 'advertiser':
+                    return JsonResponse({"ERROR":"INVALID_USER"}, status=401)
+
+                request.user = User.objects.get(email=payload['user_id'])
+
+                return func(self, request, *args, **kwargs)
+            except jwt.DecodeError:
+                return JsonResponse({"ERROR":"INVALID_TOKEN"}, status=401)
+            except User.DoesNotExist:
+                return JsonResponse({"ERROR":"ID_NOT_EXIST"}, status=401)
+        else:
+            return JsonResponse({"ERROR":"LOGIN_REQUIERD"}, status=401)
+    return decorated_function
+
+def advertiser_login_required(func):
+    def decorated_function(self, request, *args, **kwargs):
+        access_token = request.headers.get('Authorization', None)
+
+        if access_token:
+            try:
+                payload = jwt.decode(access_token, SECRET_KEY, 'HS256')
+                user_type = payload['user_type']
+
+                if user_type == 'user':
+                    return JsonResponse({"ERROR":"INVALID_USER"}, status=401)
+
+                request.user = Advertiser.objects.get(email=payload['user_id'])
+
+                return func(self, request, *args, **kwargs)
+            except jwt.DecodeError:
+                return JsonResponse({"ERROR":"INVALID_TOKEN"}, status=401)
+            except Advertiser.DoesNotExist:
+                return JsonResponse({"ERROR":"ID_NOT_EXIST"}, status=401)
+        else:
+            return JsonResponse({"ERROR":"LOGIN_REQUIERD"}, status=401)
+    return decorated_function
+
